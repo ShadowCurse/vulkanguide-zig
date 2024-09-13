029e8387527ad0716df01792e336479d4e6288b4
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-09-13 20:51:50 +0100

    small refactor of vulkan module

--- src/vulkan.zig
+++ src/vulkan.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 
-pub usingnamespace @cImport({
+const vk = @cImport({
     @cInclude("vulkan/vulkan.h");
     @cInclude("vk_mem_alloc.h");
 });
 
-const Self = @This();
+pub usingnamespace vk;
 
-pub fn check_result(result: Self.VkResult) !void {
+pub fn check_result(result: vk.VkResult) !void {
     switch (result) {
-        Self.VK_SUCCESS => return,
+        vk.VK_SUCCESS => return,
         else => {
             std.log.err("Got Vulkan error: {}", .{result});
             return error.VkError;
@@ -18,39 +18,39 @@ pub fn check_result(result: Self.VkResult) !void {
 }
 
 pub fn transition_image(
-    buffer: Self.VkCommandBuffer,
-    image: Self.VkImage,
-    source_layout: Self.VkImageLayout,
-    target_layout: Self.VkImageLayout,
+    buffer: vk.VkCommandBuffer,
+    image: vk.VkImage,
+    source_layout: vk.VkImageLayout,
+    target_layout: vk.VkImageLayout,
 ) void {
-    const aspect_mask = if (target_layout == Self.VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL)
-        Self.VK_IMAGE_ASPECT_DEPTH_BIT
+    const aspect_mask = if (target_layout == vk.VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL)
+        vk.VK_IMAGE_ASPECT_DEPTH_BIT
     else
-        Self.VK_IMAGE_ASPECT_COLOR_BIT;
-    const subresource = Self.VkImageSubresourceRange{
+        vk.VK_IMAGE_ASPECT_COLOR_BIT;
+    const subresource = vk.VkImageSubresourceRange{
         .aspectMask = @intCast(aspect_mask),
         .baseMipLevel = 0,
-        .levelCount = Self.VK_REMAINING_MIP_LEVELS,
+        .levelCount = vk.VK_REMAINING_MIP_LEVELS,
         .baseArrayLayer = 0,
-        .layerCount = Self.VK_REMAINING_ARRAY_LAYERS,
+        .layerCount = vk.VK_REMAINING_ARRAY_LAYERS,
     };
-    const barrier = Self.VkImageMemoryBarrier2{
-        .sType = Self.VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
-        .srcStageMask = Self.VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
-        .srcAccessMask = Self.VK_ACCESS_2_MEMORY_WRITE_BIT,
-        .dstStageMask = Self.VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
-        .dstAccessMask = Self.VK_ACCESS_2_MEMORY_WRITE_BIT | Self.VK_ACCESS_2_MEMORY_READ_BIT,
+    const barrier = vk.VkImageMemoryBarrier2{
+        .sType = vk.VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+        .srcStageMask = vk.VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+        .srcAccessMask = vk.VK_ACCESS_2_MEMORY_WRITE_BIT,
+        .dstStageMask = vk.VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+        .dstAccessMask = vk.VK_ACCESS_2_MEMORY_WRITE_BIT | vk.VK_ACCESS_2_MEMORY_READ_BIT,
         .oldLayout = source_layout,
         .newLayout = target_layout,
         .subresourceRange = subresource,
         .image = image,
     };
 
-    const dependency = Self.VkDependencyInfo{
-        .sType = Self.VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+    const dependency = vk.VkDependencyInfo{
+        .sType = vk.VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
         .pImageMemoryBarriers = &barrier,
         .imageMemoryBarrierCount = 1,
     };
 
-    Self.vkCmdPipelineBarrier2(buffer, &dependency);
+    vk.vkCmdPipelineBarrier2(buffer, &dependency);
 }
